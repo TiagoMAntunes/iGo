d1e1b3c6452cd03871232eb89e068479dd0e9a3f
pedro-lamego <pedrownlamego@hotmail.com>
2019-03-28 13:02:37 +0000

    messages started

--- main.js
+++ main.js
@@ -97,6 +97,7 @@ function setRealSize() {
     });
     document.getElementById('tableSettings').style.fontSize = document.body.style.fontSize;
     createNotifications();
+    createMessages();
 }
 
 
@@ -183,6 +184,14 @@ function createNotifications() {
     profiletable.innerHTML += "<tr><td id='rowone'><img class='notifpic' src=" + profiles[0].photo + ">" + profiles[0].name + " gostou da sua foto</td></tr>";
     for(i = 1; i< profiles.length; i++){   
         profiletable.innerHTML += "<tr><td class='row'><img class='notifpic' src=" + profiles[i].photo + ">" + profiles[i].name + " gostou da sua foto</td></tr>";
+    }
+}
+
+function createMessages() {
+    let profiletable = document.getElementById("messages");
+    profiletable.innerHTML += "<tr><td id='rowone'><img class='messagepic' src=" + profiles[0].photo + ">" + profiles[0].name + "</td></tr>";
+    for(i = 1; i< profiles.length; i++){
+        profiletable.innerHTML += "<tr><td class='row'><img class='messagepic' src=" + profiles[i].photo + ">" + profiles[i].name + "</td></tr>";
     }
 }
 
