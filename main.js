c80268ebd1801b1645f1d13397ed72ebc97c3743
apollee <mariabeatriz.dongamartins@gmail.com>
2019-03-30 21:46:34 +0000

    added logos to main screen

--- main.js
+++ main.js
@@ -255,7 +255,7 @@ function previousPicture() {
 
 function fillProfile() {
     let informations = document.getElementById('profileInf');
-    informations.innerHTML = "<h2 id='profileName'>" + mainprofile[0].name + "</h2><p id='profileDescr'>" + mainprofile[0].description + "</p><button class='btn-edit' onclick=\"pushScreen('profileEdit')\">Editar Perfil</button>";
+    informations.innerHTML = "<h2 id='profileName'>" + mainprofile[0].name + "</h2><p id='profileDescr'>" + mainprofile[0].description + "</p><button class='btn-edit' onclick=\"pushScreen('profileEdit')\">Edit Profile</button>";
 }
 
 function saveProfile() {
