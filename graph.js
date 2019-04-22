28b0650829c56eb84779e2f0c254cfe3683794aa
Tiago Antunes <tiago.melo.antunes@gmail.com>
2019-04-22 20:40:50 +0100

    Inserting edges

--- graph.js
+++ graph.js
@@ -7,7 +7,8 @@ class Graph {
     }
 
     insert(u, v) {
-        this.adjs[u].push(v)
+		this.adjs[u[0]].push(new Edge(v[0], u[1], u[2], v[1], v[2]))
+		this.adjs[v[0]].push(new Edge(u[0], v[1], v[2], u[1], u[2]))
     }
 }
 
@@ -40,9 +41,13 @@ class Edge{
 
 }
 
-let g = new Graph(5)
-g.insert(1,2)
-g.insert(0,3)
-g.insert(4,0)
-g.insert(4,1)
-console.log(g.adjs)
+let g = new Graph(4)
+let pin1 = [0, 1, 2]
+let pin2 = [1, 2, 5]
+let pin3 = [2, 5, 10]
+let pin4 = [3, 1, 7]
+
+g.insert(pin2,pin3)
+g.insert(pin1,pin4)
+g.insert(pin2,pin4)
+console.log(g.adjs[1])
